1c76460eab6871d1117d7b728dcbfec61a998730
emdlc <ed.delacruz@marklogic.com>
2018-01-25 22:15:09 -0500

    ReceiveJob works out of the box

--- crossdomain-core/src/main/java/com/marklogic/solutions/utils/ClasspathUtils.java
+++ crossdomain-core/src/main/java/com/marklogic/solutions/utils/ClasspathUtils.java
@@ -1,11 +1,24 @@
 package com.marklogic.solutions.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 public class ClasspathUtils {
 
     public static InputStream getClasspathContentAsStream(String path) {
         return ClasspathUtils.class.getResourceAsStream(path);
+    }
+
+    public static File getFileOrDirectoryFromClasspath(String path) {
+        return new File(ClasspathUtils.class.getResource(path).getFile());
+    }
+
+    public static Properties getPropertiesFileFromClasspath(String path) throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClasspathContentAsStream(path));
+        return properties;
     }
 
 }
--- crossdomain-core/src/test/java/com/marklogic/solutions/crossdomain/ReceiveJobTest.java
+++ crossdomain-core/src/test/java/com/marklogic/solutions/crossdomain/ReceiveJobTest.java
@@ -1,10 +1,26 @@
 package com.marklogic.solutions.crossdomain;
 
-import java.io.File;
-
+import com.marklogic.solutions.utils.ClasspathUtils;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReceiveJobTest {
+
+    @Before
+    public void setupReceiveDirectory() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        Properties testReceiveProps = ClasspathUtils.getPropertiesFileFromClasspath("/receiveJob.properties");
+
+        File testDataDir = ClasspathUtils.getFileOrDirectoryFromClasspath("/test-data");
+        File lzDir = new File(testReceiveProps.getProperty("landingzone.dir"));
+        lzDir.mkdirs();
+        FileUtils.copyDirectory(testDataDir, lzDir);
+    }
 
     @Test
     public void runReceiveOneJob() {
