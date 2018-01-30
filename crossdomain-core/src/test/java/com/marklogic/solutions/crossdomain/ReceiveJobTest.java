70a56d901091b40f19a048c9f8d7ac6795eb290d
emdlc <ed.delacruz@marklogic.com>
2018-01-29 20:38:37 -0500

    Add database assertions for ReceiveJob testing and removed Intellij project files

--- crossdomain-core/src/test/java/com/marklogic/solutions/crossdomain/ReceiveJobTest.java
+++ crossdomain-core/src/test/java/com/marklogic/solutions/crossdomain/ReceiveJobTest.java
@@ -1,31 +1,32 @@
 package com.marklogic.solutions.crossdomain;
 
+import com.marklogic.solutions.crossdomain.testutils.DatabaseAssert;
+import com.marklogic.solutions.crossdomain.testutils.LandingZoneSetupUtils;
 import com.marklogic.solutions.utils.ClasspathUtils;
-import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ReceiveJobTest {
 
-    @Before
-    public void setupReceiveDirectory() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        Properties testReceiveProps = ClasspathUtils.getPropertiesFileFromClasspath("/receiveJob.properties");
+    DatabaseAssert dbAssert;
 
-        File testDataDir = ClasspathUtils.getFileOrDirectoryFromClasspath("/test-data");
-        File lzDir = new File(testReceiveProps.getProperty("landingzone.dir"));
-        lzDir.mkdirs();
-        FileUtils.copyDirectory(testDataDir, lzDir);
+    @Before
+    public void setupLandingZoneDirectoryAndClearReplicaDatabase() throws IOException {
+        Properties testReceiveProps = ClasspathUtils.getPropertiesFileFromClasspath("/receiveJob.properties");
+        LandingZoneSetupUtils.stageTestDataToLandingZone("/test-data", testReceiveProps.getProperty("landingzone.dir"));
+        dbAssert = new DatabaseAssert(testReceiveProps.getProperty("ml.xcc.url"));
+        dbAssert.clearDatabase();
     }
 
     @Test
     public void runReceiveOneJob() {
         SimpleJobRunManager<String> mgr = new SimpleJobRunManager<String>(new ReceiveJobProcessor());
+        dbAssert.assertDocumentDoesNotExist("1234");
         JobResult result = mgr.runJob();
+        dbAssert.assertDocumentExists("1234");
         System.out.println("result startDate=" + result.getStart());
         System.out.println("result=" + result.getResultOutput());
         System.out.println("result endDate=" + result.getEnd());
