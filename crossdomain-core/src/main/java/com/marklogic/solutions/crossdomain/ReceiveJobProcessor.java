bfd7aec7d7a7e9e203e525294909a5360540ab93
loriwilhelm <lori.wilhelm@marklogic.com>
2018-01-30 21:49:48 -0500

    Update ReceiveJobProcessor.java

--- crossdomain-core/src/main/java/com/marklogic/solutions/crossdomain/ReceiveJobProcessor.java
+++ crossdomain-core/src/main/java/com/marklogic/solutions/crossdomain/ReceiveJobProcessor.java
@@ -1,134 +1,99 @@
 package com.marklogic.solutions.crossdomain;
 
-import com.marklogic.solutions.utils.ClasspathUtils;
 import com.marklogic.xcc.*;
 import com.marklogic.xcc.exceptions.RequestException;
 import com.marklogic.xcc.exceptions.XccConfigException;
-import com.marklogic.xcc.types.XName;
-import com.marklogic.xcc.types.XdmValue;
-import com.marklogic.xcc.types.XdmVariable;
-
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.jdom2.input.SAXBuilder;
-
+import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Enumeration;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import org.xml.sax.SAXException;
+import java.util.zip.ZipFile;
 import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
 import org.jdom2.output.*;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+//import org.assertj.core.util.Files;
 
 public class ReceiveJobProcessor extends JobProcessor<String> {
 	
 	private int BATCHSIZE;
 	private String landingZoneDir;
+	private String landingZoneArchiveDir;
 	private String xccURL;
 	private String collection;
 	
 	@Override
 	public JobResult executeJob() {
-
 		Date jobStartDate = new Date();
-
 		while (queue.size() > 0) {
-			// get BATCHSIZE uris from queue
-			ArrayList<String> filenames = new ArrayList<String>();
 			for (int i = 0; (i < BATCHSIZE && queue.size() > 0); i++) {
-				String filename = null;
 				try {
-					filename = queue.take();
-				} catch (InterruptedException e) {
+					String zipFileName = queue.take();
+					System.out.println("Processing file: " + zipFileName);
+					File file = new File(zipFileName);
+					String jarFileFullPath = landingZoneDir + File.separator + file.getName();
+					ZipFile zipFile = new ZipFile(jarFileFullPath);
+					Enumeration<? extends ZipEntry> entries = zipFile.entries();
+					while (entries.hasMoreElements()) {
+						ZipEntry entry = (ZipEntry) entries.nextElement();
+						if (entry.getName().endsWith(".xml")) {
+							BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+							//IOUtils.toString(bis, "UTF-8");
+							SAXBuilder builder = new SAXBuilder();
+							Document document = null;
+							document = builder.build(bis);
+							Namespace cds = Namespace.getNamespace("p","http://marklogic.com/mlcs/cds");
+							Element root = document.getRootElement();
+							String mluri = root.getChildText("UniqueIdentifier", cds);
+							String[] coll = new String[] {collection};
+							ContentCreateOptions createOptions = new ContentCreateOptions();
+							createOptions.setCollections((coll));
+							createOptions.setFormatXml();
+							DOMOutputter outputter = new DOMOutputter();
+							Content content = null;	
+							content = ContentFactory.newContent(mluri, outputter.output(document), createOptions);
+							URI uri = new URI(xccURL);
+							ContentSource source = null;
+							source = ContentSourceFactory.newContentSource(uri);
+							Session session = source.newSession();
+							session.insertContent(content);
+							bis.close();
+						}
+					}
+					zipFile.close();
+					archiveJarFile(file);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				} catch (IOException e) {
 					e.printStackTrace();
-				}
-				filenames.add(filename);
-			}
-
-			for (String filename : filenames) {
-				//read for each filename on the queue
-				FileInputStream fis = null;
-				try {
-					fis = new FileInputStream(filename);
-				} catch (FileNotFoundException e2) {
-					e2.printStackTrace();
-				}				
-				
-				//verify content is valid xml
-				SAXBuilder builder = new SAXBuilder();
-				Document document = null;
-				try {
-					document = builder.build(fis);
-				} catch (JDOMException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				Namespace cds = Namespace.getNamespace("p","http://marklogic.com/mlcs/cds");
-				Element root = document.getRootElement();
-				//get the unique identifier to use as the URI
-				String mluri = root.getChildText("UniqueIdentifier", cds);	
-				//write the xml document to the database
-				String[] coll = new String[] {collection};
-				ContentCreateOptions createOptions = new ContentCreateOptions();
-				createOptions.setCollections((coll));
-				createOptions.setFormatXml();
-				DOMOutputter outputter = new DOMOutputter();
-				Content content = null;
-				try {
-					content = ContentFactory.newContent(mluri, outputter.output(document), createOptions);
 				} catch (JDOMException e) {
 					e.printStackTrace();
-				}
-				URI uri = null;
-				try {
-					uri = new URI(xccURL);
 				} catch (URISyntaxException e) {
 					e.printStackTrace();
-				}
-				ContentSource source = null;
-				try {
-					source = ContentSourceFactory.newContentSource(uri);
 				} catch (XccConfigException e) {
 					e.printStackTrace();
-				}
-				Session session = source.newSession();
-			    try {
-					session.insertContent(content);
 				} catch (RequestException e) {
 					e.printStackTrace();
 				}
 			}
-		// close session
-
 		}
 		JobResult jobResult = new JobResult();
 		jobResult.setStart(jobStartDate);
 		jobResult.setEnd(new Date());
 		jobResult.setResultOutput("Completed ReceiveJob");
-
 		return jobResult;
-	}
-
+}
 	public ReceiveJobProcessor() {
 		super();
 		
@@ -139,14 +104,15 @@ public class ReceiveJobProcessor extends JobProcessor<String> {
 			String filename = "receiveJob.properties";
 			input = ReceiveJobProcessor.class.getClassLoader().getResourceAsStream(filename);
     		if(input==null) {
-    	            System.out.println("unable to find file:" + filename);
-    		    return;
+    			System.out.println("unable to find file:" + filename);
+    			return;
     		}
 			
 			// load a properties file
 			prop.load(input);
 			
 			this.landingZoneDir=prop.getProperty("landingzone.dir");
+			this.landingZoneArchiveDir=prop.getProperty("landingzone.archive.dir");
 			this.xccURL=prop.getProperty("ml.xcc.url");
 			this.collection=prop.getProperty("ml.collection");
 			this.BATCHSIZE=Integer.parseInt(prop.getProperty("max.batch.count"));
@@ -167,12 +133,11 @@ public class ReceiveJobProcessor extends JobProcessor<String> {
 	@Override
 	public ArrayBlockingQueue<String> retrieve(Map<String, Object> params) {
 		File[] listOfFiles = getFilesToReceive();
-        Integer n = listOfFiles.length;
+		Integer n = listOfFiles.length;
 		for (int i = 0; i < n; i++) {
 			try {
 				String fileName = listOfFiles[i].toString();
 				queue.put(fileName);
-				System.out.println(listOfFiles[i]);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -180,7 +145,7 @@ public class ReceiveJobProcessor extends JobProcessor<String> {
 		return this.queue;
 }
 
-	private File[] getFilesToReceive() {  //return changed
+	private File[] getFilesToReceive() {  
 		File[] listOfFilesToProcess = null;
 		try {
 			String dir = new String(landingZoneDir);
@@ -193,5 +158,10 @@ public class ReceiveJobProcessor extends JobProcessor<String> {
 		return listOfFilesToProcess;
     }	
 
+	private void archiveJarFile(File incomingJarFile) throws IOException {
+		String fileName = incomingJarFile.getName();
+		File archivedJarFile = new File(landingZoneArchiveDir, fileName);
+		FileUtils.copyFile(incomingJarFile,  archivedJarFile);
+		FileUtils.deleteQuietly(incomingJarFile);
+	}
 }
-
